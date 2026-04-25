a3ec4ef360ba8275bddd54a59870f6aff322eb88
oscar931220-source <oscar931220@gmail.com>
2026-04-25 18:44:33 +0800

    APlusB

--- 1/APlusB.java
+++ 1/APlusB.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package oopjudge;
 
 public class APlusB {
@@ -7,13 +6,3 @@ public class APlusB {
 	}
 
 }
-=======
-package oopjudge;
-
-public class APlusB {
-	public static int plus(int a, int b) {
-		return a+b;
-	}
-
-}
->>>>>>> c3d32ee0a79ed74188c7dfe629454cbbc08dc4c5
