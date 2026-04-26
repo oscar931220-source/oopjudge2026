6edcc567b6465a88cc5a5dd0c484de9acecdf67c
oscar931220-source <oscar931220@gmail.com>
2026-04-26 14:17:38 +0800

    Update APlusB.java

--- 1/APlusB.java
+++ 1/APlusB.java
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 public class APlusB {
 	public static int plus(int a, int b) {
 		return a+b;
 	}
 
 }
+=======
+public class APlusB {
+	public static int plus(int a, int b) {
+		return a+b;
+	}
+
+}
+>>>>>>> Stashed changes
