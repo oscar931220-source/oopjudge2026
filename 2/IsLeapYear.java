12b0913fe67ee59be889ee2b16f97a1236e136ed
oscar931220-source <oscar931220@gmail.com>
2026-04-28 21:10:49 +0800

    Update IsLeapYear.java

--- 2/IsLeapYear.java
+++ 2/IsLeapYear.java
@@ -1,11 +1,9 @@
 public class IsLeapYear {
 
     public boolean determine(int year) {
-        if (year % 400 == 0) {
+        if (year % 4 == 0) {
             return true;
-        } else if (year % 4 == 0 && year % 100 != 0) {
-            return true;
-        } else {
+        }else {
             return false;
         }
     }
