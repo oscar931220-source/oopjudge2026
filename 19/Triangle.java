a6099571197e4cdbfd3d0ef3de6c92d040bd20a5
oscar931220-source <oscar931220@gmail.com>
2026-06-17 01:28:13 +0800

    Create Triangle.java

---
+++ 19/Triangle.java
@@ -0,0 +1,43 @@
+/**
+ * The Triangle class represents a regular triangle.
+ */
+public class Triangle extends Shape {
+
+    /**
+     * Constructs a triangle with the given side length.
+     *
+     * @param length the side length of the triangle
+     */
+    public Triangle(double length) {
+        super(length);
+    }
+
+    /**
+     * Sets the side length of this triangle.
+     *
+     * @param length the side length of the triangle
+     */
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    /**
+     * Gets the area of this regular triangle.
+     *
+     * @return the area of this triangle
+     */
+    public double getArea() {
+        double area = Math.sqrt(3) / 4.0 * length * length;
+        return Math.round(area * 100.0) / 100.0;
+    }
+
+    /**
+     * Gets the perimeter of this regular triangle.
+     *
+     * @return the perimeter of this triangle
+     */
+    public double getPerimeter() {
+        double perimeter = 3.0 * length;
+        return Math.round(perimeter * 100.0) / 100.0;
+    }
+}
