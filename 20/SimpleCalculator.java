8c4d2fa62e4a393cf52a84c13e8ed9fe55637539
oscar931220-source <oscar931220@gmail.com>
2026-06-18 20:33:13 +0800

    Update SimpleCalculator.java

--- 20/SimpleCalculator.java
+++ 20/SimpleCalculator.java
@@ -8,6 +8,7 @@ public class SimpleCalculator {
     private int count;
     private String operator;
     private double value;
+    private boolean ended;
 
     /**
      * Constructs a SimpleCalculator with result initialized to 0.
@@ -17,6 +18,7 @@ public class SimpleCalculator {
         count = 0;
         operator = "";
         value = 0.0;
+        ended = false;
     }
 
     /**
@@ -26,6 +28,18 @@ public class SimpleCalculator {
      * @throws UnknownCmdException if the command is invalid
      */
     public void calResult(String cmd) throws UnknownCmdException {
+        if (cmd == null || cmd.length() == 0) {
+            throw new UnknownCmdException("Please enter 1 operator and 1 value separated by 1 space");
+        }
+
+        if (cmd.indexOf(" ") == -1) {
+            if (cmd.length() == 1 && !isOperator(cmd)) {
+                throw new UnknownCmdException(cmd + " is an unknown value");
+            } else {
+                throw new UnknownCmdException("Please enter 1 operator and 1 value separated by 1 space");
+            }
+        }
+
         String[] parts = cmd.split(" ", -1);
 
         if (parts.length != 2 || parts[0].length() != 1 || parts[1].length() == 0) {
@@ -35,7 +49,7 @@ public class SimpleCalculator {
         String op = parts[0];
         String valStr = parts[1];
 
-        boolean validOperator = op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/");
+        boolean validOperator = isOperator(op);
         boolean validValue = true;
         double number = 0.0;
 
@@ -47,9 +61,13 @@ public class SimpleCalculator {
 
         if (!validOperator && !validValue) {
             throw new UnknownCmdException(op + " is an unknown operator and " + valStr + " is an unknown value");
-        } else if (!validOperator) {
+        }
+
+        if (!validOperator) {
             throw new UnknownCmdException(op + " is an unknown operator");
-        } else if (!validValue) {
+        }
+
+        if (!validValue) {
             throw new UnknownCmdException(valStr + " is an unknown value");
         }
 
@@ -73,6 +91,16 @@ public class SimpleCalculator {
     }
 
     /**
+     * Checks whether the given string is a valid operator.
+     *
+     * @param op the operator string
+     * @return true if the operator is valid, false otherwise
+     */
+    private boolean isOperator(String op) {
+        return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/");
+    }
+
+    /**
      * Gets the message of the current calculation result.
      *
      * @return the message of the current result
@@ -80,15 +108,21 @@ public class SimpleCalculator {
     public String getMsg() {
         DecimalFormat df = new DecimalFormat("0.00");
 
+        if (ended) {
+            return "Final result = " + df.format(result);
+        }
+
         if (count == 0) {
             return "Calculator is on. Result = " + df.format(result);
-        } else if (count == 1) {
+        }
+
+        if (count == 1) {
             return "Result " + operator + " " + df.format(value) + " = " + df.format(result)
                     + ". New result = " + df.format(result);
-        } else {
-            return "Result " + operator + " " + df.format(value) + " = " + df.format(result)
-                    + ". Updated result = " + df.format(result);
         }
+
+        return "Result " + operator + " " + df.format(value) + " = " + df.format(result)
+                + ". Updated result = " + df.format(result);
     }
 
     /**
@@ -98,6 +132,11 @@ public class SimpleCalculator {
      * @return true if the command is r or R, false otherwise
      */
     public boolean endCalc(String cmd) {
-        return cmd.equals("r") || cmd.equals("R");
+        if (cmd.equals("r") || cmd.equals("R")) {
+            ended = true;
+            return true;
+        }
+
+        return false;
     }
 }
